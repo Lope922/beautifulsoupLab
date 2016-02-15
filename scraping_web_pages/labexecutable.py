ef1a99bf243d16f678bf13eccad6da99092ecef1
Lope <Lope922@gmail.com>
2016-02-15 00:27:20 -0600

    ran code through python was able to get it functioning

--- scraping_web_pages/labexecutable.py
+++ scraping_web_pages/labexecutable.py
@@ -1,34 +1,36 @@
-import requests 
+import requests
+from requests.exceptions import ConnectionError
 from bs4 import BeautifulSoup 
 # userful commands for lab 4 and star tribune website 
 
 def go():
 
-    r = requests.get("http://www.startribune.com")
-
-    soup = beautifulSoup(r.content)
-
-    print(soup.printify())
+    print("program has started. Fetching a headline....")
 
 
+    r = requests.get("http://www.startribune.com", timeout=3)
+    soup = BeautifulSoup(r.content, "html5lib")
+    return soup
+# call everything in go
 
-# thought this was helpful for looking at other code... 
-# prints the html or js in a readable format. 
 
-# call everything in go 
-go()
+def display_headline(soup):
+    title = soup.find("a", "tease-headline").string
+    print(title)
+    print("end")
 
-# other commands soup.find_all("a")
 
-# evolved into this thus far 
-#soup.find_all(class_="tease-headline", limit = 1)
 
-#now trying to narrow down to just data-linkname
+try:
+    soup = go()
+    display_headline(soup)
 
-# command that was able to get things tidied up in the end 
-soup.find("a", "tease-headline").string
+except ConnectionError as connErr:
+    print("Check your internet connection and try again. \n Detailed information : ", connErr)
+except Exception as e:
+    print("Something went wrong. Report bug to developer. Here is detailed information\n", e)
 
-print("end")
+# command to pull headline from star tribune website
 
 #upon reviewing the documentation i was able to find that If a tag has only one child, and that child is a NavigableString, 
 #the child is made available as .string:
